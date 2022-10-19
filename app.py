61d2dbed904431553d04a8e4295401ff137182bb
Peter Vidos <116172604+vizzu-streamlit@users.noreply.github.com>
2022-10-19 17:55:07 +0200

    Add new story

--- app.py
+++ app.py
@@ -1,87 +1,103 @@
 from streamlit.components.v1 import html
 import pandas as pd
-from ipyvizzu import Chart, Data, Config, Style
+from ipyvizzu import Data, Config, Style
+from ipyvizzustory import Story, Slide, Step
 import ssl
 import streamlit as st 
 
 ssl._create_default_https_context = ssl._create_unverified_context  
-# Codes shamelessly adapted from here 
-# - https://github.com/alod83/data-science/blob/master/DataVisualization/ipyvizzu/ipyvizzu%20Example.ipynb
+
 st.set_page_config(page_title="ipyvizzu-story in Streamlit", layout="wide")
-st.sidebar.title("ipyvizzu-story Demos")
-st.title("Streamlit :balloon: + ipyvizzu 📈🎬🚀")
-st.sidebar.button("Animate ♻️ ")
+st.sidebar.title("Poll results - Presentation tools")
+st.title("Streamlit 🎈 + ipyvizzu-story 📈🎬🚀")
+
 def create_chart():
     # initialize chart
-    chart = Chart(width="100%", height="100%", display="manual")
-    # add data
     data = Data()
+    df = pd.read_csv("Data/Poll_results.csv")
+    data.add_data_frame(df)
+    #@title Create the story
     
-    data_frame = pd.read_csv('Data/eu_regions.csv',sep=';')
-    data.add_data_frame(data_frame)
+    story = Story(data=data)
+    story.set_size("100%", "100%")
 
-    chart.animate(data)
-
-    chart.animate(
-        Config({
-                "channels": {
-                    "y": "Country",
-                    "x": ["Population (2020)","Subregion"]                    
-                }
-            }),
+    slide1 = Slide(
+        Step( 
             Style({
-                "plot":{"paddingLeft": "12em"}
-            })
-            ,y={
-            "duration": 3,
-            
-        }
-    )
-
-    chart.animate(
-        Config(
-            {
-                    "y": None,
-                    "x": None,
-                    "size": ["Country","Population (2020)"],
-                    "label": "Country",
-                    "color": "Subregion",
-                    "geometry": "circle"
-            }
-        )
-    )
-    chart.animate(
-        Config({
-                "y": "Subregion",
-                "x": ["Country","Population (2020)"],
-                "label": None,
-                "size" : None,
-                "geometry": "rectangle"
-            })        
-    )
-    
-    chart.animate(
-        Config({
-             "x": "Population (2020)",
-            "label": "Population (2020)"
+                "legend": {"label": {"fontSize": "1.1em"}, "paddingRight": "-1em"},
+                "plot": { 
+                    "marker": { "label": { "fontSize": "1.1em"}}, 
+                    "paddingLeft": "10em",
+                    "xAxis": {"title": { "color": "#00000000"}, "label": { "fontSize": "1.1em"}},
+                    "yAxis": {"label": { "fontSize": "1.1em"}}},
+                "logo": {"width": "6em"}
             }),
+            Config.stackedBar({
+                "x": "Vote percentage [%]",
+                "y": "Group number",
+                "stackedBy": "Answer",
+                "title": "Stacked Bar Chart"
+            })
         )
-    
+    )
+    story.add_slide(slide1)
 
-       
-    return chart._repr_html_(),data_frame
+    slide2 = Slide(
+    Step(
+        Style({ "plot": { "xAxis": { "label": { "color": "#00000000"}}}}),
+        Config({ "split": True, "title": "Splitted Bar chart"})
+    )
+    )
+    story.add_slide(slide2)
+
+    slide3 = Slide(
+    Step(
+        Style({ "plot": { "marker": { "label": { "fontSize": "0.916667em"}}}}),
+        Config({ "x": {"set": ["Vote count","Answer"]}, "label": "Vote count", "title": "Splitted bar chart - vote count"}),
+    )
+)
+    story.add_slide(slide3)
+
+    slide4 = Slide()
+    slide4.add_step(
+        Step(
+            Style({ "plot": { "yAxis": { "title": { "color": "#00000000"}}}}),
+            Config({ "x": "Answer", "y": ["Group number","Vote count"], "split": False, "legend": "color"})
+        )
+    )
+
+    slide4.add_step(
+        Step(
+            Style({ "plot": { "marker": { "label": { "fontSize": "1.1em"}}}}),
+            Config({ "y": "Vote count", "title": "Column chart"}),
+        )
+    )
+    story.add_slide(slide4)
+
+    slide5 = Slide()
+    slide5.add_step(
+        Step(
+            Config({ "x": ["Answer percentage [%]","Answer"], "y": None, "label":"Answer percentage [%]"})
+        )
+    )
+
+    slide5.add_step(
+        Step(
+            Style({ "plot": { "xAxis": {"label": {"color": "#00000000"}}}}),
+            Config({ "coordSystem": "polar", "title":"Pie chart"})
+        )
+    )
+    story.add_slide(slide5)
+    
+    # Switch on the tooltip that appears when the user hovers the mouse over a chart element.
+    story.set_feature("tooltip", True)
+
+    return story._repr_html_(),df
 
 
 CHART,df = create_chart()
-html(CHART, width=700, height=600)
-with st.expander(label = "Resources", expanded=False):
-        st.markdown("### Data")
-        st.dataframe(df)
-        st.markdown('''
-                    ### Code
-                    [Link to original code](https://github.com/alod83/data-science/blob/master/DataVisualization/ipyvizzu/ipyvizzu%20Example.ipynb)
-                    
-                    [ipyvizzu](https://github.com/vizzuhq/ipyvizzu)
-                    
-                    ''')
-        
+html(CHART, width=700, height=450)
+
+st.markdown('''
+            [Source](https://twitter.com/VizzuHQ/status/1575473747599007744)
+            ''')    
